5623a82e5edb38d47e1c0a2837693ff81e9a7acd
Rocio <rociobonindev@gmail.com>
2025-07-07 21:42:26 -0300

    feat: define entities for PlatformUser, Store, Member, and Role with TypeORM decorators

--- src/modules/_platform/stores/entities/store.entity.ts
+++ src/modules/_platform/stores/entities/store.entity.ts
@@ -1,1 +1,24 @@
-export class Store {}
+import { Column, CreateDateColumn, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+export class Store {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column({ nullable: false })
+    name: string;
+
+    @Column({ nullable: false, unique: true })
+    domain: string;
+
+    @Column({ default: true })
+    isActive: boolean;
+
+    @Column()
+    platformUserId : string;
+
+    @CreateDateColumn()
+    createdAt: Date;
+
+    @UpdateDateColumn()
+    updatedAt: Date;
+}
