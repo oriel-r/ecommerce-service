aa0336a24e32e2bc5da478945b0d791f57e92100
Oriel Romero <orielromero97@gmail.com>
2025-07-31 23:18:15 -0300

    fix(seeders): now the rol was created in seeders

--- src/database/seeding/platform-seeder/platform-user.seeder.ts
+++ src/database/seeding/platform-seeder/platform-user.seeder.ts
@@ -4,6 +4,7 @@ import { DataSource } from "typeorm";
 import { PlatformUsersService } from "src/modules/_platform/platform-users/platform-users.service";
 import { platformUserMock } from "../mocks";
 import { StoreSeeder } from "../store-seeder/store.seeder";
+import { RolesService } from "src/modules/auth/roles/roles.service";
 
 @Injectable()
 export class PlatformUserSeeder {
@@ -13,14 +14,17 @@ export class PlatformUserSeeder {
 
     constructor(
         private readonly platformUserService: PlatformUsersService,
-        private readonly storeSeeder: StoreSeeder
+        private readonly storeSeeder: StoreSeeder,
+        private readonly rolesService: RolesService
     ) {}
     
     async seedAdminUser() {
         try {
+            await this.rolesService.createRoleIfNotExists('platform')
             const newUser = await this.platformUserService.create(this.platformUser)
             this.logger.log('Se añadio el usuario admin a la base de datos')
         } catch (error) {
+            console.log(error)
             if(error instanceof ConflictException) {
                 this.logger.log('El usuarío admin ya esta registrado')
             } else {
